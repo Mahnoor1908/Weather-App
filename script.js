1fed734b0e710e9f823a00da8db418bcc7ede7b2
Muhammad Arham <67412607+Arham-12336@users.noreply.github.com>
2024-05-01 17:59:48 +0500

    added more city

--- script.js
+++ script.js
@@ -1,76 +1,71 @@
+
 const weather_element = document.getElementById("weather");
 const temperature_main_element = document.getElementById("temperature-main");
-const weather_description_element = document.getElementById("weather-description");
+console.log("temperature main element: ", temperature_main_element);
+const weather_description_element = document.getElementById(
+  "weather-description"
+);
+
 const weather_icon_element = document.getElementById("icon");
 const temperature_element = document.getElementById("temperature");
 const humidity_element = document.getElementById("humidity");
 const wind_speed_element = document.getElementById("wind-speed");
+console.log("wind element: ", wind_speed_element);
 const country_element = document.getElementById("country");
 const town_element = document.getElementById("city");
+const sunset_element = document.getElementById("sunset-main");
+const sunrise_element = document.getElementById("sunrise-main");
 
+const cityNameByid = (id) => {
+  if (id == "1") {
+    return "karachi";
+  } else if (id == "2") {
+    return "Islamabad";
+  } else if (id == "3") {
+    return "Lahore";
+  } else if (id == "4") {
+    return "Washington";
+  } else if (id == "5") {
+    return "Multan";
+  } else if (id == "6") {
+    return "Toronto";
+  } else if (id == "7") {
+    return "Peshawar"
+  }
+};
 
-const cityNameByid=(id)=>{
-    if(id=='1')
-    {
-        return "karachi";
-    }
-    else if(id=='2')
-    {
-        return "Islamabad";
-    }
-    else if(id=='3')
-    {
-        return "Lahore"
-    }
+async function updatingValues(data) {
+  temperature_main_element.innerText = await `${data.main.temp} °C`;
+  weather_element.innerText = await data.weather[0].main;
+  wind_speed_element.innerText = await `${data.wind.speed}m/s`;
+  humidity_element.innerText = await `${data.main.humidity}%`;
+  country_element.innerText = await data.sys.country;
+  temperature_element.innerText = await `${data.main.temp} °C`;
+  town_element.innerText = await data.name;
+  weather_description_element.innerText = await data.weather[0].description;
+  sunset_element.innerText = await `${new Date(data.sys.sunset*1000)}`;
+  sunrise_element.innerText = await `${new Date(data.sys.sunrise*1000)}`;
 }
 
-function getValue(){
-    const selectElement=document.querySelector('#dropdown');
-    let output = selectElement.value;
-    var city=cityNameByid(output);
-    console.log("value ",city)
-    const apiKey = "b8ccdd1fdfff90aad2c6effb84a10369";
-    const apiURL = `https://api.openweathermap.org/data/2.5/weather?&appid=${apiKey}&q=${city}&units=metric`;
 
-fetch(apiURL)
-.then(async (response) =>{
-    if (!response.ok){
+function getValue() {
+  const selectElement = document.querySelector("#dropdown");
+  let output = selectElement.value;
+  var city = cityNameByid(output);
+  const apiKey = "b8ccdd1fdfff90aad2c6effb84a10369";
+  const apiURL = `https://api.openweathermap.org/data/2.5/weather?&appid=${apiKey}&q=${city}&units=metric`;
+
+  fetch(apiURL)
+    .then(async (response) => {
+      if (!response.ok) {
         alert("An error occured");
-        return
-    } 
+        return;
+      }
 
-    return response.json();
-
-})
-.then(async (data) => {
-    console.log("Weather Data:" , await data);
-    const weather=data.weather[0].main;
-    const weather_description= data.weather[0].description;
-    const weather_icon =data.weather[0].icon;
-    const temperature_main =data.main.temp;
-    const humidity =data.main.humidity
-    const wind_speed =data.wind.speed;
-    const country = data.sys.country;
-    const town =data.name;
-
-    console.log("weather: ",weather)
-    console.log("weather_description: ",weather_description)
-    console.log("weather_icon: ",weather_icon)
-    console.log("temperature_main: ",temperature_main)
-    console.log("humidity: ",humidity)
-    console.log("wind_speed: ",wind_speed)
-    console.log("country: ",country)
-    console.log("town: ",town)
-
-
-    weather_element.innerText =await weather;
-    country_element.innerText =await country;
-    town_element.innerText =await town;
-    temperature_main_element.innerText =await temperature;
-    weather_description_element.innerText =await weather_description;
-    document.getElementById('humidity').innerText = "arham";
-    wind_speed_element =await `${wind_speed}m/s`;
-    temperature_main_element.innerText = `${temperature_main}°C`;
-}); 
+      return response.json();
+    })
+    .then(async (data) => {
+      updatingValues(data);
+      console.log("Weather Data:", await data);
+    });
 }
-   
